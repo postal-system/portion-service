69dd47efcbefa35641acc2964436f6c062c66b62
Alexander Smirnov <asmirnov@codero.io>
2022-02-11 16:35:26 +0600

    PEJT-28: added project

--- src/main/kotlin/io/codero/portionservice/service/PortionProducerService.kt
+++ src/main/kotlin/io/codero/portionservice/service/PortionProducerService.kt
@@ -2,7 +2,6 @@ package io.codero.portionservice.service
 
 import io.codero.portionservice.dto.PortionDto
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
@@ -10,13 +9,8 @@ import org.springframework.stereotype.Service
 class PortionProducerService(val kafkaTemplate: KafkaTemplate<String, PortionDto>) {
     private val log = LoggerFactory.getLogger(javaClass)
 
-//    @Value("\${spring.kafka.producer.portion-topic}")
-//    private val topic: String? = null
-
     fun send(dto: PortionDto) {
-//        if (topic != null) {
-            kafkaTemplate.send("portions", dto)
-            log.info("#### <- {}", dto)
-//        }
+        kafkaTemplate.send("portions", dto)
+        log.info("#### <- {}", dto)
     }
 }
